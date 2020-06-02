c99d54bda7b8ca31495978ca6aecbf9758108e9a
Emre <emre@edakik.com>
2020-06-02 21:59:38 +0300

    dto lar yazıldı.

---
+++ tools/dtos/activity/activity-type.dto.ts
@@ -0,0 +1,6 @@
+import { AuditDto } from "../common/audit.dto";
+
+export class ActivityTypeDto {
+    name: string;
+    audit: AuditDto;
+}
---
+++ tools/dtos/user/user.dto.ts
@@ -0,0 +1,8 @@
+export class UserDto {
+    id: string;
+    name: string;
+    surname: string;
+    password: string;
+    email: string;
+    birthDate: Date;
+}
