58577732a4713619a7f5013d93e2ca95a01adf4d
Emre <emre@edakik.com>
2020-06-02 23:57:51 +0300

    user için servisler yazıldı

---
+++ src/user/user.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { UserDto } from 'tools/dtos/user/user.dto';
+import { UserService } from './user.service';
+import { CreateUserDto } from 'tools/dtos/user/create-user.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  findAll(): UserDto[] {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): UserDto {
+    return this.userService.findOne(id);
+  }
+
+  @Post()
+  post(@Body() createUserDto: CreateUserDto): UserDto {
+    return this.userService.createUser(createUserDto);
+  }
+}
